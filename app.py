5d918a8da43cd31b81668d697d1cd8e0673636e9
José Enrique <politeles@users.noreply.github.com>
2022-01-19 14:09:53 +0100

    first working version
    this is the first iteration on the template

--- app.py
+++ app.py
@@ -2,10 +2,21 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE in the project root for license information.
 #-----------------------------------------------------------------------------------------
+import sagemaker
+from flask import Flask,render_template
+from transformers import pipeline;
+import os
+import boto3
 
-from flask import Flask
 app = Flask(__name__)
+
+def test_hugging_face():
+    return pipeline('sentiment-analysis')('we love you')
+
+hf_data = test_hugging_face()
+
 
 @app.route("/")
 def hello():
-    return app.send_static_file("index.html")
+    #testing hugging face library
+    return render_template("index.html",data=hf_data)
